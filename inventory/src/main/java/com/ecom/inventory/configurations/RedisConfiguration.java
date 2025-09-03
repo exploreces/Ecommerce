ab7eb7563c96b59166aec5e08de5e3f0a9a4dff7
Ayushi Sarkar <ayushi_sarkar@epam.com>
2025-09-03 19:22:33 +0530

    redis cluster added

--- inventory/src/main/java/com/ecom/inventory/configurations/RedisConfiguration.java
+++ inventory/src/main/java/com/ecom/inventory/configurations/RedisConfiguration.java
@@ -8,11 +8,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
+import java.util.Arrays;
 
 @Configuration
 public class RedisConfiguration {
@@ -44,6 +47,25 @@ public class RedisConfiguration {
                 .build();
     }
 
+    /**
+     * Configure RedisConnectionFactory to work with a Redis Cluster.
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // Define the Redis Cluster configuration with cluster nodes
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration(
+                Arrays.asList(
+                        "127.0.0.1:7001",
+                        "127.0.0.1:7002",
+                        "127.0.0.1:7003",
+                        "127.0.0.1:7004",
+                        "127.0.0.1:7005",
+                        "127.0.0.1:7006"
+                )
+        );
+        // Create LettuceConnectionFactory for Redis Cluster
+        return new LettuceConnectionFactory(clusterConfiguration);
+    }
 
 
 }
